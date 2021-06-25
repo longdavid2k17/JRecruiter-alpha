b6ff772179ba2a6e4afd90793b836b77cd313e8d
longdavid2k17 <kantochdawid98@gmail.com>
2021-06-25 13:28:54 +0200

    First commit, works api with JWT

---
+++ src/main/java/com/example/jrecruiteralpha/JRecruiterAlphaApplication.java
@@ -0,0 +1,13 @@
+package com.example.jrecruiteralpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JRecruiterAlphaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JRecruiterAlphaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jrecruiteralpha/JRecruiterAlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jrecruiteralpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JRecruiterAlphaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
